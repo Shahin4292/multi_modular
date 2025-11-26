e1f5f18c5e541e83b0920dba85777d9b125c7729
shahin <sarker4292@gmail.com>
2025-11-27 01:22:26 +0600

    added dependency file on app_seettings

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,3 +1,4 @@
+import 'package:app_settings/di/injection.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:multi_modular/di/injection.config.dart';
@@ -5,4 +6,7 @@ import 'package:multi_modular/di/injection.config.dart';
 final getIt = GetIt.instance;
 
 @InjectableInit()
-void configureDependencies() => getIt.init();
+void configureDependencies(String? environment){
+  getIt.init(environment: environment);
+  configureAppSettingsDependencies(getIt, environment);
+}
