38b3f8add3ec4ef337ffedf82edb893b4e409b2e
shahin <sarker4292@gmail.com>
2025-11-30 12:36:21 +0600

    configure dioFactory

--- core/data/lib/constants/constants.dart
+++ core/data/lib/constants/constants.dart
@@ -1,7 +1,9 @@
-const String applicationJson = "application/json";
 const String contentType = "content-type";
 const String accept = "accept";
 const String authorization = "authorization";
+
+// Value of the headers
+const String applicationJson = "application/json";
 
 // custom headers
 const String defaultLanguage = "language";
---
+++ core/data/lib/factory/dio_factory.dart
@@ -0,0 +1,36 @@
+import 'package:data/constants/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory();
+
+  Future<Dio> getDio()async{
+    Dio dio = Dio();
+    Map<String, String> headers = {
+      contentType: applicationJson,
+      accept: applicationJson,
+      authorization: "your_access_token", // todo get access token here
+      defaultLanguage: "app language", // todo get app language here
+      clientId: "app_client_id", // todo get client id here
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: "your_base_url", // todo get base url from config
+      headers: headers,
+      receiveTimeout: const Duration(seconds: 60),
+      sendTimeout: const Duration(seconds: 60),
+    );
+
+    // add logger in debug mode only Not released yet
+    if(!kReleaseMode){
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader : true,
+        requestBody : true,
+        responseHeader : true,
+      ));
+    }
+    return dio;
+  }
+}
