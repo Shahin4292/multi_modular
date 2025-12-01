ab3552d44a327cb84fc92b9d47a3fbf34d4d3922
shahin <sarker4292@gmail.com>
2025-12-01 19:21:10 +0600

    Converting Dio Error Responses into Meaningful Failure Messages

--- core/domain/lib/model/failure.dart
+++ core/domain/lib/model/failure.dart
@@ -1,9 +1,8 @@
-import 'package:domain/model/localised_message.dart';
 
 // error model that we expect from api
 class Failure {
   int code; // 200, 201, 303, 404, 400, 500
-  LocalisedMessage message;
+  String message;
   Failure(this.code, this.message);
 
 }
