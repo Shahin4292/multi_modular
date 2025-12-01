b3c92cce6b095097add2e2523d3246c338c47ab8
shahin <sarker4292@gmail.com>
2025-12-01 17:56:02 +0600

    added AppStrings

---
+++ core/data/lib/error_handler/error_messages.dart
@@ -0,0 +1,15 @@
+class AppStrings{
+  static const String success = "success";
+  static const String noContent = "no_content";
+  static const String badRequest = "bad_request";
+  static const String forbidden = "forbidden";
+  static const String unauthorized = "unauthorized";
+  static const String internalServerError = "internal_server_error";
+  static const String connectionTimeOut = "connection_time_Out";
+  static const String canceled = "canceled";
+  static const String receiveTimeOut = "receive_time_Out";
+  static const String sendTimeOut = "send_time_Out";
+  static const String cacheError = "cache_error";
+  static const String noInternetConnection = "no_internet_connection";
+  static const String defaultError = "default_error";
+}
