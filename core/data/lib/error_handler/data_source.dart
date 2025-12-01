829212692489113c7cdb43b013e3a6fcd466cc7b
shahin <sarker4292@gmail.com>
2025-12-01 17:30:27 +0600

    added datasource enum

---
+++ core/data/lib/error_handler/data_source.dart
@@ -0,0 +1,14 @@
+enum DataSource {
+  success,
+  noContent,
+  badRequest,
+  unauthorized,
+  internalServerError,
+  connectionTimeout,
+  canceled,
+  receiveTimeout,
+  sendTimeout,
+  cacheError,
+  noInternetConnection,
+  defaultError,
+}
